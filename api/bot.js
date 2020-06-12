bee7b7d34d42a65302ea9bb3cc6cd8bc52e0e7f5
Sidor Gautamov <sidorgautamov@gmail.com>
2020-06-12 17:58:36 +0400

    🚀🌌 Updated with VSCode

--- api/bot.js
+++ api/bot.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 
 const router = express.Router();
 const access_token = process.env.access_token;
-const group_id = proces.env.group_id;
+const group_id = process.env.group_id;
 
 router.get('/bot/callback', (req, res, next) => {
     res.status(200).json({ Status: res.statusCode });
@@ -14,7 +14,9 @@ router.post('/bot/callback', (req, res, next) => {
     res.status(200).json({ Status: res.statusCode });
     const { name, number } = req.body;
     const message = `Имя: ${name} Номер: ${number}`;
-    fetch(`https://api.telegram.org/bot${access_token}/senMessage?chat_id=${group_id}&text=${message}`);
+    fetch(`https://api.telegram.org/bot${access_token}/sendMessage?chat_id=${group_id}&text=${encodeURIComponent(message)}`, { method: 'POST' })
+        .then(res => res.json())
+        .then(json => console.log(json));
     res.end();
 });
 
